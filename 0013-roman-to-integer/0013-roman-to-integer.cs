2161119d2b67c01e6c42923fc71f494e6a775bd2
Mohamed Emad Abbas <111843015+m7md3bbas@users.noreply.github.com>
2026-08-24 18:44:48 +0300

    Time: 3 ms (69.13%), Space: 50.3 MB (60.61%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        var map = new Dictionary<char, int> {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int sum = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i < s.Length - 1 && map[s[i]] < map[s[i + 1]])
+            {
+                sum -= map[s[i]];   
+            }
+            else
+            {
+                sum += map[s[i]]; 
+            }
+        }
+
+        return sum;
+    }
+}
