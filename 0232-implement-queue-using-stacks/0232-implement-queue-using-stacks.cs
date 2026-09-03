24bfad205dcfc88dc9953f607726a46514e8c7d1
Mohamed Emad Abbas <111843015+m7md3bbas@users.noreply.github.com>
2026-09-03 11:02:24 +0300

    Time: 1 ms (95.88%), Space: 44.1 MB (10.9%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cs
@@ -0,0 +1,46 @@
+public class MyQueue
+{
+    private Stack<int> stack1;
+    private Stack<int> stack2;
+
+    public MyQueue()
+    {
+        stack1 = new Stack<int>();
+        stack2 = new Stack<int>();
+    }
+
+    public void Push(int x)
+    {
+        stack1.Push(x);
+    }
+
+    public int Pop()
+    {
+        MoveElements();
+
+        return stack2.Pop();
+    }
+
+    public int Peek()
+    {
+        MoveElements();
+
+        return stack2.Peek();
+    }
+
+    public bool Empty()
+    {
+        return stack1.Count == 0 && stack2.Count == 0;
+    }
+
+    private void MoveElements()
+    {
+        if (stack2.Count == 0)
+        {
+            while (stack1.Count > 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+        }
+    }
+}
